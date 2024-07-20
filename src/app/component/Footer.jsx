684c34e8706bd880eb4a15b9b1a79526abe0428c
KiwNanthawat2149 <nanthawat_si@cmu.ac.th>
2024-07-20 20:00:41 +0700

    hb

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(props) {
   return (
     <div className="mt-3">
     <p className="text-secondary text-center">
-      copyright © {props.year} {props.fullname} {props.studentId}
+      copyright © {props.year} {props.fullname} {props.studentID}
     </p>
   </div>
 
